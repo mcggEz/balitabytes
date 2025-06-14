cd5014d5970c9dfe4131407790f22f55ab52272f
mcggEz <112741425+mcggEz@users.noreply.github.com>
2025-06-14 14:29:18 +0800

    feat: added news card

---
+++ app/components/NewsCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NewsCard = () => {
+  return (
+    <div className="bg-blue-200 p-5 h-50 w-50 rounded-md">
+    <div>Title</div>
+    <div>Description</div>
+    <div>Image</div>
+    <div>Link</div>
+  </div>
+  )
+}
+
+export default NewsCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,60 @@
-import Image from "next/image";
+
+  type NewsItem = {
+    id: number;
+    title: string;
+    description: string;
+    imageUrl: string;
+    link: string;
+  }
+
+  const newsItems: NewsItem[] = [
+    {
+      id: 1,
+      title: "News 1",
+      description: "Description 1",
+      imageUrl: "https://via.placeholder.com/150",
+      link: "https://www.google.com"
+    },
+    {
+      id: 2,
+      title: "News 2",
+      description: "Description 2",
+      imageUrl: "https://via.placeholder.com/150",
+      link: "https://www.google.com"
+    }
+  ] 
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  
+   <>
+
+      <div className="bg-red-500 p-5">
+      <div className="text-4xl font-bold">Balitabytes</div>
+      </div>
+      <div className="flex flex-col gap-2 justify-center items-center">
+      <div className="text-2xl font-bold p-5">Welcome to Balitabytes</div>
+      <div className="text-sm text-gray-500">Today's date is {new Date().toLocaleDateString()}</div>
+      <input type="date" />
+      <div>
+      <input className="border-2 border-gray-300 rounded-md p-2" type="text" placeholder="So what news do you want to hear about" />
+      
+      <button className="bg-blue-500 text-white p-2 rounded-md">Search</button>
+      </div>
+      </div>
+        {/* news component */}
+        <div className="p-5 flex gap-2">
+          {newsItems.map((newsItem) => (
+            <div className="bg-blue-200 p-5 h-50 w-50 rounded-md hover:bg-blue-300"  key={newsItem.id}>
+            <div>Title: {newsItem.title}</div>
+            <div>Description: {newsItem.description}</div>
+            <div>Image: {newsItem.imageUrl}</div>
+            <div>Link:  <a href={newsItem.link}>{newsItem.link}</a></div>
+          </div>  
+          ))}
+       
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+   </>
   );
 }
